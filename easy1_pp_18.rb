2485533e4a42076c9a9e3c5a00f4fc93b52557de
serenahks <112879871+serenahks@users.noreply.github.com>
2023-04-14 10:37:52 -0400

    Create easy1_pp_18.rb

---
+++ easy1_pp_18.rb
@@ -0,0 +1,8 @@
+#Given the hash: flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+# Create an array containing only two elements: Barney's name and Barney's number.
+
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+
+p barney_arr = ["Barney", flintstones["Barney"]]
+
+puts barney_arr
